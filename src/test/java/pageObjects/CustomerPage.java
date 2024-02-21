db4a2c0bfc7788c1bb68b3af1c84d98007a51ea0
jamespaulr <jmspaulr@gmail.com>
2024-02-21 15:57:46 +0530

    Project Commit

--- src/test/java/pageObjects/CustomerPage.java
+++ src/test/java/pageObjects/CustomerPage.java
@@ -4,12 +4,10 @@ import org.apache.commons.lang.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.Random;
 
-<<<<<<< HEAD
 public class CustomerPage
 {
     String title;
@@ -48,163 +46,112 @@ public class CustomerPage
 
     public String getPageTitle() throws InterruptedException {
         Thread.sleep(5000);
-            title= ldriver.getTitle();
+        title= ldriver.getTitle();
         return title;
-=======
-import static org.openqa.selenium.By.xpath;
-
-public class CustomerPage {
-    WebDriver ldriver;
-
-    public CustomerPage(WebDriver rdriver) {
-        ldriver = rdriver;
-        PageFactory.initElements(rdriver, this);
->>>>>>> 4c5e12f1d653422d6a5b5b8684ca75bc90d52584
     }
 
-    @FindBy(xpath = "/html/body/div[3]/aside/div/div[4]/div/div/nav/ul/li[4]/a/p/i")
-    WebElement Customer_menu;
-    @FindBy(xpath = "//p[text()=' Customers']")
-    WebElement Customer;
-    @FindBy(xpath = "//a[@href=\"/Admin/Customer/Create\"]")
-    WebElement addNewCust;
-
-    @FindBy(id = "Email")
-    WebElement Email;
-    @FindBy(id = "Password")
-    WebElement Password;
-    @FindBy(id = "FirstName")
-    WebElement FirstName;
-    @FindBy(id = "LastName")
-    WebElement LastName;
-    @FindBy(id = "Gender_Male")
-    WebElement Gender_Male;
-    @FindBy(id = "Gender_Female")
-    WebElement Gender_Female;
-    @FindBy(id = "DateOfBirth")
-    WebElement DateOfBirth;
-    @FindBy(id = "Company")
-    WebElement CompanyName;
-    @FindBy(id = "IsTaxExempt")
-    WebElement IsTaxExempt;
-    @FindBy(id = "SelectedNewsletterSubscriptionStoreIds_listbox")
-    WebElement SelectedNewsletterSubscriptionStoreIds_listbox;
-    @FindBy(xpath = "//div[@class='k-widget k-multiselect k-multiselect-clearable k-state-hover']/child::div")
-    WebElement customer_role;
-    @FindBy(xpath = "//li[text()='Administrators']")
-    WebElement Administrator;
-    @FindBy(xpath = "//li[text()='Forum Moderators']")
-    WebElement Forum_Moderators;
-    @FindBy(xpath = "//li[text()='Guests']")
-    WebElement Guests;
-    @FindBy(xpath = "//li[text()='Registered']")
-    WebElement Registered;
-    @FindBy(xpath = "//li[text()='Vendors']")
-    WebElement Vendors;
-    @FindBy(xpath = "//option[text()='Not a vendor']")
-    WebElement Not_a_Vendor;
-    @FindBy(xpath = "//option[text()='Vendor 1']")
-    WebElement Vendor_1;
-    @FindBy(xpath = "//option[text()='Vendor 2']")
-    WebElement Vendor_2;
-    @FindBy(id = "Active")
-    WebElement Active_checkbox;
-    @FindBy(id = "AdminComment")
-    WebElement Admin_comment;
-    @FindBy(xpath = "//button[@name='save']")
-    WebElement Save;
-
-
-
-        public void clickOnCustomerList ()
+    public void clickOnCustomerList()
+    {
+        ldriver.findElement(Customer_menu).click();
+    }
+    public void clickOnCustomerMenu()
+    {
+        ldriver.findElement(Customer).click();
+    }
+    public void clickOnNewCust()
+    {
+        ldriver.findElement(addNewCust);
+    }
+    public String emailID;
+    public void RandomiseString()
+    {
+        String Alphabet= RandomStringUtils.random(6);
+        emailID = Alphabet.concat("@gmail.com");
+    }
+    public void enterEmail(String emailID)
+    {
+        ldriver.findElement(Email).clear();
+        ldriver.findElement(Email).sendKeys(emailID);
+    }
+    public void enterPassword(String pwd)
+    {
+        ldriver.findElement(Password).clear();
+        ldriver.findElement(Password).sendKeys(pwd);
+    }
+    public void enterFirstname(String fName)
+    {
+        ldriver.findElement(FirstName).clear();
+        ldriver.findElement(FirstName).sendKeys(fName);
+    }
+    public void enterLastname(String Lname)
+    {
+        ldriver.findElement(LastName).clear();
+        ldriver.findElement(LastName).sendKeys(Lname);
+    }
+    public void selectGender(String gender)
+    {
+        if(gender.equals("male"))
         {
-            Customer_menu.click();
+            ldriver.findElement(Gender_Male).click();
         }
-
-        public void clickOnCustomerMenu ()
+        else if (gender.equals("female"))
         {
-            Customer.click();
+            ldriver.findElement(Gender_Female).click();
         }
-        public void clickOnNewCust ()
+        else
         {
-            addNewCust.click();
+            ldriver.findElement(Gender_Male).click();
         }
-        public String emailID;
-        public void RandomiseString ()
+    }
+    public void DOB(String dob)
+    {
+        ldriver.findElement(DateOfBirth).sendKeys(dob);
+    }
+    public void enterCompanyName(String companyName)
+    {
+        ldriver.findElement(CompanyName).sendKeys(companyName);
+    }
+    public void setCustomerRole(String role)
+    {
+        ldriver.findElement(customer_role).click();
+        if (role.equals("Administrator"))
         {
-            String Alphabet = RandomStringUtils.random(6);
-            emailID = Alphabet.concat("@gmail.com");
+            ldriver.findElement(Administrator).click();
         }
-        public void enterEmail (String emailID)
+        else if (role.equals("Forum Moderators"))
         {
-            Email.clear();
-            Email.sendKeys(emailID);
-        }
-        public void enterPassword (String pwd)
+            ldriver.findElement(Forum_Moderators).click();
+        } else if (role.equals("Guests"))
         {
-           Password.clear();
-           Password.sendKeys(pwd);
-        }
-        public void enterFirstname (String fName)
+            ldriver.findElement(Guests).click();
+        } else if (role.equals("Registered"))
         {
-            FirstName.clear();
-            FirstName.sendKeys(fName);
+            ldriver.findElement(Registered).click();
         }
-        public void enterLastname (String Lname)
+        else if (role.equals("Vendors"))
         {
-           LastName.clear();
-           LastName.sendKeys(Lname);
+            ldriver.findElement(Vendors).click();
         }
-        public void selectGender (String gender)
+    }
+    public void managerVendor(String Vendor)
+    {
+        if (Vendor.equals("Vendor1"))
         {
-            if (gender.equals("male")) {
-                Gender_Male.click();
-            } else if (gender.equals("female")) {
-                Gender_Female.click();
-            } else {
-                Gender_Male.click();
-            }
+            ldriver.findElement(Vendor_1).click();
         }
-        public void DOB (String dob)
+        else
         {
-            DateOfBirth.sendKeys(dob);
+            ldriver.findElement(Vendor_2).click();
         }
-        public void enterCompanyName (String companyName)
-        {
-           CompanyName.sendKeys(companyName);
-        }
-        public void setCustomerRole (String role)
-        {
-            customer_role.click();
-            if (role.equals("Administrator")) {
-                Administrator.click();
-            } else if (role.equals("Forum Moderators")) {
-                Forum_Moderators.click();
-            } else if (role.equals("Guests")) {
-                Guests.click();
-            } else if (role.equals("Registered")) {
-                Registered.click();
-            } else if (role.equals("Vendors")) {
-                Vendors.click();
-            }
-        }
-        public void managerVendor (String Vendor)
-        {
-            if (Vendor.equals("Vendor1")) {
-                Vendor_1.click();
-            } else {
-                Vendor_2.click();
-            }
-        }
-        public void enterAdmin_comment (String adminComments)
-        {
-            Admin_comment.sendKeys(adminComments);
-        }
-        public void clickSave ()
-        {
-            Save.click();
-        }
-
-
+    }
+    public void enterAdmin_comment(String adminComments)
+    {
+        ldriver.findElement(Admin_comment).sendKeys(adminComments);
+    }
+    public void clickSave()
+    {
+        ldriver.findElement(Save);
     }
 
+
+}
